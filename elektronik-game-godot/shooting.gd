19b47eb33e255a85a95840aece6b20c13f0f61c7
Elrabu <busch.elias@web.de>
2026-02-10 22:13:44 +0100

    fixed alignement issue

--- elektronik-game-godot/shooting.gd
+++ elektronik-game-godot/shooting.gd
@@ -1,11 +1,13 @@
 extends Node2D
 
 @export var laser_scene: PackedScene
+@onready var shoot = %shoot
 
 var can_player_shoot = true
 
 func _input(_event):
 	if Input.is_action_just_pressed("shoot") && can_player_shoot:
+		shoot.play()
 		can_player_shoot = false
 		var laser = laser_scene.instantiate() as Laser
 		laser.global_position = get_parent().global_position - Vector2(0, 20)
--- elektronik-game-godot/start_screen.gd
+++ elektronik-game-godot/start_screen.gd
@@ -6,25 +6,33 @@ extends CanvasLayer
 @onready var invader_2_label = %Invader2Label
 @onready var invader_3_texture = %Invader3Texture
 @onready var invader_3_label = %Invader3Label
-@onready var button = %Button
+@onready var button = $MarginContainer/VBoxContainer/HBoxContainer/Button
 @onready var timer = $Timer
 
 var control_array = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	control_array.append_array([invader_1_texture, invader_1_label, invader_2_texture, invader_2_label, invader_3_texture, invader_3_label, button])
-	
-	for control in control_array:
-		(control as Control).visible = false
+	control_array = [
+		[invader_1_texture, invader_1_label],
+		[invader_2_texture, invader_2_label],
+		[invader_3_texture, invader_3_label],
+		[button]
+	]
+
+	for entry in control_array:
+		for control in entry:
+			(control as Control).visible = false
 
 func load_game():
 	get_tree().change_scene_to_file("res://main.tscn")
 
 func show_next_control():
-	var control = control_array.pop_front() as Control
-	if control != null:
-		control.visible = true
-	else:
+	if control_array.is_empty():
 		timer.stop()
 		timer.queue_free()
+		return
+
+	var entry = control_array.pop_front()
+	for control in entry:
+		(control as Control).visible = true
