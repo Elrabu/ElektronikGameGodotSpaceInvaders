403d2660eca88b7c0d3f448047620faa69ca7548
Elrabu <busch.elias@web.de>
2026-02-23 12:46:30 +0100

    adjusted size

--- elektronik-game-godot/player.gd
+++ elektronik-game-godot/player.gd
@@ -17,7 +17,7 @@ var start_bound
 var end_bound
 
 func _ready():
-	position.y = 486
+	position.y = 400
 	bounding_size_x = collision_rect.shape.get_rect().size.x
 	
 	var rect = get_viewport().get_visible_rect()
